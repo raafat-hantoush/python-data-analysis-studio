db8b8a0b60f681dada3f007d1cea6e9565f757b1
Rafa <raafat.hantoush@gmail.com>
2022-02-10 14:36:11 +0100

    init files

---
+++ web_app/py_data_analysis_code_generator/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PyDataAnalysisCodeGeneratorConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'py_data_analysis_code_generator'

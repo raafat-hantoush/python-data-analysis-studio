21389903e22daea826709aa5c703cd4569094a49
Rafa <raafat.hantoush@gmail.com>
2022-02-10 17:13:51 +0100

    init files

--- web_app/py_data_analysis_code_generator/views.py
+++ web_app/py_data_analysis_code_generator/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpResponse
+from django.template import loader
 from django.shortcuts import render
+import pandas as pd
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the Code Genrator index.")
+    df=pd.DataFrame({"name":["Kike","Nelson","Rafa"],"Age":[19,18,22]})
+    print(df.to_html())
+    template = loader.get_template('/py_data_analysis_code_generator/index.html')
+    return HttpResponse("<b> Hello Kike")
+    ##return render(request, 'py_data_analysis_code_generator/index.html')    
+    ##return HttpResponse(template.render(request))

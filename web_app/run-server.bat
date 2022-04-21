ebd882aff130ee3468b1b35546bb26cb59b504b0
Rafa <raafat.hantoush@gmail.com>
2022-04-21 17:28:07 +0200

    batch file edit

--- web_app/run-server.bat
+++ web_app/run-server.bat
@@ -1,5 +1,6 @@
 ::cd "$(dirname "$0")"
 cd /D "%~dp0"
+@pause
 :: conda activate mlstudio
 :: python manage.py runserver
 :: open http://localhost:8000/mlstudio
--- web_app/run-server.command
+++ web_app/run-server.command
@@ -1,4 +1,5 @@
 #! /bin/bash
 cd "$(dirname "$0")"
+$SHELL
 #python manage.py runserver
 #open http://localhost:8000/mlstudio
